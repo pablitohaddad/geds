1360a748a203bdd5c51387e5d69c68d6235cede5
AnaPaula-dev <anapaularodriguesmachado2005@gmail.com>
2025-08-13 18:21:06 -0400

    tarefa ana

--- Aula 4 - Calculadora Spring PT1/src/main/java/io/github/pablitohaddad/HelloWorld/service/CalculadoraService.java
+++ Aula 4 - Calculadora Spring PT1/src/main/java/io/github/pablitohaddad/HelloWorld/service/CalculadoraService.java
@@ -30,6 +30,16 @@ public class CalculadoraService {
     }
 
     // TODO -> Ana Paula divisão
+    public double dividir(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Não é possível dividir por zero");
+        }
+        double resultado = num1 / num2;
+        Operacao operacao = new Operacao(num1, num2, "/", resultado);
+        historico.add(operacao);
+        return resultado;
+    }
+
 
     // TODO -> Geisiane módulo
     /* presumi que módulo se refere a (num1%num2) e
